04b7328f56d533c51c94e1468e3256cef5ddb6f9
Abner Santos <88912494+abnerpsantos@users.noreply.github.com>
2023-07-31 19:26:40 -0300

    Create valiadation for unkown type of response body

--- src/ui/repository/todoRepository.ts
+++ src/ui/repository/todoRepository.ts
@@ -19,7 +19,8 @@ async function get({
     limit,
 }: TodoRepositoryGetParams): Promise<TodoRepositoryGetOutput> {
     const data = await fetch("/api/todos");
-    const { todoList }: { todoList: Array<Todo> } = await data.json();
+    const jsonData = await data.json();
+    const { todoList } = parseTodo(jsonData);
     const totalOfTodos = todoList.length;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
@@ -30,6 +31,26 @@ async function get({
         totalOfTodos,
         pages: totalOfTodos / limit,
     };
+}
+
+function parseTodo(responseBody: unknown): { todoList: Array<Todo> } {
+    if (
+        responseBody !== null &&
+        typeof responseBody === "object" &&
+        "todoList" in responseBody &&
+        Array.isArray(responseBody.todoList)
+    ) {
+        return {
+            todoList: responseBody.todoList.map((todo) => {
+                if (todo === null && typeof todo !== "object") {
+                    throw new Error("Invalid todo from api");
+                }
+                const { id, content, date, done } = todo;
+                return { id, content, date, done } as Todo;
+            }),
+        };
+    }
+    throw new Error("Invalid todo from api");
 }
 
 export default {
